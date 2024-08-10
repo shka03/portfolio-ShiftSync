1faa18410589631576ddf40ae82df77adab52c7a
shka03 <katsurashigeki0325@gmail.com>
2024-08-10 18:53:25 +0900

    ログイン動作作成の更新。

--- src/main/java/com/levels/ShiftSync/config/SecurityConfig.java
+++ src/main/java/com/levels/ShiftSync/config/SecurityConfig.java
@@ -11,20 +11,30 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 	// SecurityFilterChainのBean定義
-	 @Bean
-	 public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	 http
-		// ★HTTPリクエストに対するセキュリティ設定
-		.authorizeHttpRequests(authz -> authz
-		// 「/login」へのアクセスは認証を必要としない
-		.requestMatchers("/login").permitAll()
-		// その他のリクエストは認証が必要
-		.anyRequest().authenticated())
-		// ★フォームベースのログイン設定
-		.formLogin(form -> form
-		// カスタムログインページのURLを指定
-		.loginPage("/login")
-		);
-	 	return http.build();
-	 }
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			// ★HTTPリクエストに対するセキュリティ設定
+			.authorizeHttpRequests(authz -> authz
+			// 「/login」へのアクセスは認証を必要としない
+			.requestMatchers("/login").permitAll()
+			// その他のリクエストは認証が必要
+			.anyRequest().authenticated())
+			// ★フォームベースのログイン設定
+			.formLogin(form -> form
+			// カスタムログインページのURLを指定
+			.loginPage("/login")
+			// ログイン処理のURLを指定
+			.loginProcessingUrl("/authentication")
+			// ユーザー名のname属性を指定
+			.usernameParameter("usernameInput")
+			// パスワードのname属性を指定
+			.passwordParameter("passwordInput")
+			// ログイン成功時のリダイレクト先を指定
+			.defaultSuccessUrl("/")
+			// ログイン失敗時のリダイレクト先を指定
+			.failureUrl("/login?error")
+			);
+		return http.build();
+	}
 }
