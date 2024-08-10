e89d075a97459c5f24d0f37aa248f99ffbc5be4d
shka03 <katsurashigeki0325@gmail.com>
2024-08-10 19:03:46 +0900

    ログイン用の認証情報エンティティクラスの作成完了。

---
+++ src/main/java/com/levels/ShiftSync/entity/Authentication.java
@@ -0,0 +1,17 @@
+package com.levels.ShiftSync.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Authentication {
+	/** ユーザー名 */
+	private String username;
+	/** パスワード */
+	private String password;
+	/** 権限 */
+	private Role authority;
+}
