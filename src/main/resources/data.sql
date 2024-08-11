209c0142f6d3d3bfe7f6d9f8e0497fa9177ab93c
shka03 <katsurashigeki0325@gmail.com>
2024-08-11 15:57:21 +0900

    ログイン機能のPW機能の修正完了。

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 -- 認証テーブルへのダミーデータの追加
 -- password：adminpass
 INSERT INTO authentications (username, password, authority)
-VALUES ('admin', 'adminpass', 'ADMIN');
+VALUES ('admin', '$2a$10$4ws3NzLezXw7nxFY4MRdOuGEqpKEPRmAwRAMnJGvAUOqdagyLd376', 'ADMIN');
 -- password：userpass
 INSERT INTO authentications (username, password, authority)
 VALUES ('user', '$2a$10$/jar9xXQ6lrnVjLvLGv5BepFkLnGIO49RrGx42p2i.1hQt1BZ/7E2', 'USER');
