33d7c625e17ee4510a00c736826a3ceb14fd2132
shka03 <katsurashigeki0325@gmail.com>
2024-08-17 20:50:54 +0900

    当日の出退勤時間を取得するMapperを作成。

--- src/main/java/com/levels/ShiftSync/repository/AttendanceRecordMapper.java
+++ src/main/java/com/levels/ShiftSync/repository/AttendanceRecordMapper.java
@@ -14,6 +14,9 @@ public interface AttendanceRecordMapper {
     // 従業員の退勤時間を登録する
     void clockOut(AttendanceRecord record);
     
-    // 従業員の退勤時間を登録する
+    // 従業員の当日の出退勤時間を取得する。
+    List<AttendanceRecord> getTodayAttendance(Integer employeeId);
+    
+    // 従業員の当月の出退勤時間を取得する
     List<AttendanceRecord> getMonthlyAttendance(Integer employeeId);
 }
