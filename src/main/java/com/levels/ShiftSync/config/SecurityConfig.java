62f95c43ef5416606f3f01fbe663f728e80ff7f7
shka03 <katsurashigeki0325@gmail.com>
2024-08-28 23:23:19 +0900

    PROJ-88 不要なコードを削除。

--- src/main/java/com/levels/ShiftSync/config/SecurityConfig.java
+++ src/main/java/com/levels/ShiftSync/config/SecurityConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
@@ -14,10 +12,6 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-	/** DI対象が存在すれば、DIして使用する */
-	 private final UserDetailsService userDetailsService;
-	 private final PasswordEncoder passwordEncoder;
-	
 	// SecurityFilterChainのBean定義
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
