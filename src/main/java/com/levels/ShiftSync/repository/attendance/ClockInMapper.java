d1ca1a407ef1bf96a6b7fdfc6939403a4c1f43cc
shka03 <katsurashigeki0325@gmail.com>
2024-09-07 12:47:53 +0900

    PROJ-101 リファクタリング。

---
+++ src/main/java/com/levels/ShiftSync/repository/attendance/ClockInMapper.java
@@ -0,0 +1,8 @@
+package com.levels.ShiftSync.repository.attendance;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ClockInMapper extends RecordMapperProt {
+
+}
---
+++ src/main/java/com/levels/ShiftSync/repository/attendance/ClockOutMapper.java
@@ -0,0 +1,8 @@
+package com.levels.ShiftSync.repository.attendance;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ClockOutMapper extends RecordMapperProt {
+	
+}
